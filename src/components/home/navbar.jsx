b3ea25a762e55af584bd31e9d0302b48979c7dd0
Onur <onur.onel2230@gmail.com>
2025-08-30 17:02:03 -0400

    Added about us

--- src/components/home/navbar.jsx
+++ src/components/home/navbar.jsx
@@ -14,6 +14,7 @@ import { PlusGrid, PlusGridItem, PlusGridRow } from './plus-grid'
 const links = [
   { href: '/docs', label: 'Newsletters' },
   { href: '/podcasts', label: 'Podcasts' },
+  { href: '/about', label: 'About Us' },
   { href: '/login', label: 'Sign Up' },
 ]
 
