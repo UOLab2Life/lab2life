3a5a3dcfdee1465c6cee10d25ef67f22aa00087a
nahiyani <nahiyan.ishtiaque@gmail.com>
2025-11-27 12:41:11 -0500

    Dev (#96)
    * new podcast + article + ui updates
    
    * updated articles page layout
    
    * incorporating vercel analytics into website
    
    installing vercel's website analytics
    
    * murder mystery event update
    
    * november article #2
    
    * made changes to not show forensic event

--- src/components/home/navbar.jsx
+++ src/components/home/navbar.jsx
@@ -178,11 +178,11 @@ function MobileNav() {
 export function Navbar({ showBanner = true, shortText = false }) {
   const { t, locale } = useTranslation()
   
-  const bannerHref = 'https://www.bouncelife.com/events/691cacfdf171eb798e4a8425'
+  const bannerHref = getLocalizedUrl('/articles/forensic-scientists-bring-justice', locale)
   const bannerText =
     locale === 'fr'
-      ? 'NOUVEAU ÉVÉNEMENT !: Meutre au gala'
-      : 'NEW EVENT!: Murder at the Gala'
+      ? 'ARTICLE: Les scientifiques légistes mettent la science au service de la justice'
+      : 'ARTICLE: How Forensic Scientists Bring Science Into Justice'
 
   const defaultBanner = (
     <Link
